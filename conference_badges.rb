1888f2af8eea5ef3f93e18f9ce13e8974d7b61a4
Mark Rego <rego121@gmail.com>
2019-08-15 00:59:06 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+def badge_maker_creator(array)
+  array = ['Edsger', 'Ada', 'Charles', 'Alan, Grace', 'Linus', 'Matz']
+
+  array.each |speaker| do
+
+    puts "hello #{array}"
+
+  end
+
+  def assign_rooms(array)
+    room = 1
+    arr =  ['Edsger', 'Ada', 'Charles', 'Alan, Grace', 'Linus', 'Matz']
+
+    array.each do |room|
+
+      puts "Hello, #{arr}! You'll be assigned to room #{room}!"
+      room += 1
+
+    end
+
+
+
+      
